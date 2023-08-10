57b9dec04c35ad585e45fc0832610934ae3131bf
Mandeep_S <mandeep.singh08@dellteam.com>
2023-08-10 14:36:13 +0530

    Ading Solution

---
+++ TheOrganizer/MainService.cs
@@ -0,0 +1,14 @@
+﻿namespace TheOrganizer
+{
+    internal class MainService
+    {
+        static void Main(string[] args)
+        {
+            // one listerner service
+
+            // listener activates other service 
+
+
+        }
+    }
+}
